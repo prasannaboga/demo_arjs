5b07b0bc28bb6f34ee2309ae5bcc326530c198e0
prasanna.boga <prasannaboga@gmail.com>
2018-04-02 17:06:18 +0530

    Added webcodecam jquery

--- config.rb
+++ config.rb
@@ -45,7 +45,7 @@ set :relative_links, true
 
 configure :build do
   activate :minify_css
-  activate :minify_javascript
+  activate :minify_javascript, ignore: [/webcodecamjquery/]
 end
 
 activate :deploy do |deploy|
